69214dd0748866c5feb28bbb6963eb2a4f086614
GM89 <gennismestres@gmail.com>
2021-12-02 15:50:27 +0100

    bug solved

--- server/server.js
+++ server/server.js
@@ -1,23 +1,7 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-
-
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => 
-  console.log(`Server running on port ${port}`
-  ));
-
-
-
-//Send message to the html
-app.get('/', (req, res) => res.send('Hello world!'));
-
-
-
-//// Middleware----------------------------------------------
-
+const db = require("./keys").mongoURI;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -31,37 +15,17 @@ app.use(cors());
 
 
 
-//route ------------------------------------------------------
-app.use('/cities', require('./routes/cities'));
-app.use('/all', require('./routes/cities'));
+//routes
 
-//----------------------------------------------------------------
 
-/*
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://genis123:admin123@sandbox.h6gff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+app.listen(port, () => {
+  console.log("Server is running on " + port + "port");
 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-// */
-
-
-
-
-
-// db es el modulo mongoURI del archivo keys.js --------------------------------
-const db = require('./keys').mongoURI;
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://genis123:admin123@sandbox.h6gff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true })
-    .then(() => console.log('Connection to Mongo DB established!'))
-    .catch(err => console.log(err));
-
-////
+mongoose.connect(db, { 
+  
+})
+    .then(() => console.log('Connection to Mongo DB established'))
+    .catch(err => console.log(err));
