f6820c58d68d347bfaa019b969d6ba036124bfc9
Genis <gennismestres@gmail.com>
2022-02-01 13:07:20 +0100

    authentication local OK, googleauth NOT OK

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,27 +16,33 @@ async function loginUser(credentials) {
 
 
  function Login({ setToken }) {
-  const [name, setUserName] = useState();
-  const [password, setPassword] = useState();
-  const [email, setUserEmail]= useState();
+   const[user, setUser] = useState({
+     userName:"",
+     email:"",
+     password:"",
+   });
+
 
   async function registerUser(event) {
     /* prevent the default behaviour caused by the event itself.
      Keep it from attempting to refresh the browser as the browser
     tries to submit the form to some back end server that doesn't exist */
     event.preventDefault()
-    await fetch('http://localhost:5000/api/register', {
-      method: 'POST',
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        email, 
-        password,
-      })
-      .then(data => data.json())
-    })
+    
+    await fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify({
+          userName: user.name,
+          email: user.email, 
+          password: user.password,
+        })
+      }).then(data => data.json())
+
+      console.log("user has been registered")
+
   }
 
 //This doesn't work
@@ -49,26 +55,25 @@ async function loginUser(credentials) {
     <div>
   
      
-      <GoogleAuthButton/>
-  
-    
+     {/*  <GoogleAuthButton/> */}
+
     <div className="login-wrapper">
-      <h1>Register</h1>
-      <form onSubmit={registerUser}>
+      <h1>Login</h1>
+      <form onSubmit={e=> registerUser(e)}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)} />
+          <input type="text" onChange={e => setUser((x)=>({ ...x, userName:e.target.value,}))} />
         </label>< br/>
         <label>
           <p>Email</p>
-          <input type="email" onChange={e => setUserEmail(e.target.value)} />
+          <input type="email" onChange={e => setUser((x)=>({ ...x, email:e.target.value,}))}  />
         </label>< br/>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
+          <input type="password" onChange={e => setUser((x)=>({ ...x, password:e.target.value,}))}  />
         </label>< br/>
         <div>
-          <button type="submit">Submit</button>
+          <button  type="submit">Submit</button>
         </div>
       </form>
     </div>
